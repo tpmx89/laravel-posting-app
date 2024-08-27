b4b4ece9dd1a603e2c087be7bb49186c5dbd7d41
tpmx89 <tpmx.d.728qyu@icloud.com>
2024-08-27 12:48:10 +0900

    dummy data

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +43,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+         // 1人のユーザーは複数の投稿を作成できる
+         public function posts()
+         {
+             return $this->hasMany(Post::class);
+         }
 }
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostsrFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 2,
+            'title' => fake()->realText(),
+            'content' => fake()->realText()            
+            //
+        ];
+    }
+}
